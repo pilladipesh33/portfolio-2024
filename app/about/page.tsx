47c3b96c60ac5eb810cf362e02680f3a56b784dc
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-05-02 12:44:32 +0530

    initial commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -36,12 +36,30 @@ const About = () => {
         </div>
       </section>
 
-      <section className="pt-5">
+      <section className="pt-5 ">
         <h4 className="text-sm tracking-[1.86px] leading-[1.2em] text-[#afb2b9] pb-10 uppercase">
           info
         </h4>
-        <BreakSpace />
-        <p></p>
+        <p className="text-sm text-[#3b3d41] tracking-normal leading-5">
+          {AboutList.CONTENT_1}
+        </p>
+        <p className="text-sm text-[#3b3d41] tracking-normal leading-5 pt-5">
+          {AboutList.CONTENT_2}
+        </p>
+        <p className="text-sm text-[#3b3d41] tracking-normal leading-5 pt-5">
+          {AboutList.CONTENT_3}
+        </p>
+      </section>
+
+      <div className="border-white border border-b-[#ecedf0] pb-20"></div>
+
+      <section className="pt-5">
+        <h4 className="text-sm tracking-[1.86px] leading-[1.2em] text-[#afb2b9] pb-5 uppercase">
+          education
+        </h4>
+        <div className="relative h-48 w-full rounded-lg">
+          <Image alt="uni" src={"/uni.jpeg"} layout="fill" objectFit="cover" />
+        </div>
       </section>
     </div>
   );
