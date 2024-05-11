9f996dd2d850b7cf8c7b4f82d012a04090050728
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-05-11 11:27:47 +0530

    working on idea page

--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const Ideas = () => {
   return (
-    <div className="mt-20 mx-6">
+    <div className="mt-20 mx-6 md:mt-0">
       <h1 className="pt-10 text-xs tracking-[1.86px] text-[#afb2b9]">
         SPECULATIVE
       </h1>
-      <h1 className="text-3xl tracking-[-.01em] text-[#3b3d41] pt-5 leading-9 font-normal">
+      <h1 className="text-3xl tracking-[-.01em] text-[#3b3d41] pt-5 leading-9 font-bricolage md:text-4xl md:font-semibold">
         Ideas about web
       </h1>
       <p className="text-[14px] leading-5 pt-5 text-[#3b3d41] tracking-normal font-sans pb-10">
---
+++ lib/fonts.ts
@@ -0,0 +1,7 @@
+import { Bricolage_Grotesque } from "next/font/google";
+
+const bircolage = Bricolage_Grotesque({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bricolage",
+});
